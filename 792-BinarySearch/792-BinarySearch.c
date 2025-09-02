c2ade661433d6ea78ec439d2087600947a6acd2c
Abhilash Puli <sherlockholmes9886@gmail.com>
2025-09-02 13:41:30 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.c
@@ -0,0 +1,17 @@
+// Last updated: 9/2/2025, 1:41:32 PM
+int search(int* nums, int numsSize, int target) {
+    int low=0;
+    int high=numsSize-1;
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+        if(target==nums[mid])
+        return mid;
+        else if(target>nums[mid])
+        low=mid+1;
+        else
+        high=mid-1;
+    }
+    return -1;
+    
+}

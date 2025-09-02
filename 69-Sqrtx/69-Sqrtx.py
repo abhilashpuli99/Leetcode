b4cd17afed2d0695fdb0c671f7e88f923bc0bf2f
Abhilash Puli <sherlockholmes9886@gmail.com>
2025-09-02 13:43:37 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.py
@@ -0,0 +1,10 @@
+# Last updated: 9/2/2025, 1:43:38 PM
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        import math as m
+        a=m.sqrt(x)
+        return int(a)

b4b70e12c8944f42c07303be63c1abfd09cd4edf
Abhilash Puli <sherlockholmes9886@gmail.com>
2025-09-02 13:40:35 +0530

    Create 3903-InverseCoinChange

---
+++ 3903-InverseCoinChange/3903-InverseCoinChange.py
@@ -0,0 +1,15 @@
+# Last updated: 9/2/2025, 1:40:37 PM
+class Solution:
+    def findCoins(self, dp: List[int]) -> List[int]:
+        n = len(dp)
+        dp = [1] + dp
+        res = []
+        for a in range(1, n + 1):
+            if dp[a] > 1: return []
+            if dp[a] == 0: continue
+            res.append(a)
+            for v in range(n, a - 1, -1):
+                dp[v] -= dp[v - a]
+                if dp[v] < 0:
+                    return []
+        return res      

f6e17e7150f7a3d0a2f04975ece0b2cda4173f94
Abhilash Puli <sherlockholmes9886@gmail.com>
2025-09-02 13:41:04 +0530

    Create 1660-ThousandSeparator

---
+++ 1660-ThousandSeparator/1660-ThousandSeparator.py
@@ -0,0 +1,26 @@
+# Last updated: 9/2/2025, 1:41:06 PM
+class Solution(object):
+    def thousandSeparator(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        num=str(n)
+        if(len(num)<=3):
+            return str(n)
+        else:
+            num=num[::-1]
+            ele=""
+            for i in range (len(num)):
+                if i%3==0:
+                    ele+='.'
+                    ele+=num[i]
+                else:
+                    ele+=num[i]
+        
+        return ele[::-1].strip('.')
+
+
+
+
+        

dc2cb73e429217938e6776b5bc6edc208e093d4d
Abhilash Puli <sherlockholmes9886@gmail.com>
2025-09-02 13:41:41 +0530

    Create 547-NumberOfProvinces

---
+++ 547-NumberOfProvinces/547-NumberOfProvinces.py
@@ -0,0 +1,18 @@
+# Last updated: 9/2/2025, 1:41:43 PM
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def dfs(i):
+            for j in range(len(isConnected)):
+                if isConnected[i][j] and not visited[j]:
+                    visited[j]=True
+                    dfs(j)
+        n=len(isConnected)
+        visited=[False]*n
+        count=0
+        for i in range(len(isConnected)):
+            if not visited[i]:
+                count+=1
+                visited[i]=True
+                dfs(i)
+        return count
+        #TRy bfs

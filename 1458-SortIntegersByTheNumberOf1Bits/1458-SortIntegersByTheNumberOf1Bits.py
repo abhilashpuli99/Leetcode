468220d72f4ddec30a088bb4a410c50651176d1d
Abhilash Puli <sherlockholmes9886@gmail.com>
2025-09-02 13:41:13 +0530

    Create 1458-SortIntegersByTheNumberOf1Bits

---
+++ 1458-SortIntegersByTheNumberOf1Bits/1458-SortIntegersByTheNumberOf1Bits.py
@@ -0,0 +1,9 @@
+# Last updated: 9/2/2025, 1:41:15 PM
+class Solution(object):
+    def sortByBits(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        return sorted(arr, key=lambda x: (bin(x).count('1'), x))
+    

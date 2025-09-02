6dfcf0c0bf6803e7e93175ca60ee04ebf49042d6
Abhilash Puli <sherlockholmes9886@gmail.com>
2025-09-02 13:40:44 +0530

    Create 3708-ZigzagGridTraversalWithSkip

---
+++ 3708-ZigzagGridTraversalWithSkip/3708-ZigzagGridTraversalWithSkip.py
@@ -0,0 +1,26 @@
+# Last updated: 9/2/2025, 1:40:46 PM
+class Solution:
+    def zigzagTraversal(self, grid: List[List[int]]) -> List[int]:
+        m=len(grid)
+        n=len(grid[0])
+        result=[]
+        if not grid:
+            return result
+        k=1
+        for i in range(1,m+1):
+            if i%2!=0:
+                for j in range(1,n+1):
+                    if k%2!=0 and i%2!=0:
+                        result.append(grid[i-1][j-1])
+                    elif j%2==0 and i%2==0:
+                        result.append(grid[i-1][j-1])
+                    k+=1
+            elif i%2==0:
+                for j in range(n,0,-1):
+                    if k%2!=0 and i%2!=0:
+                        result.append(grid[i-1][j-1])
+                    elif j%2==0 and i%2==0:
+                        result.append(grid[i-1][j-1])
+                    k+=1
+        return result
+

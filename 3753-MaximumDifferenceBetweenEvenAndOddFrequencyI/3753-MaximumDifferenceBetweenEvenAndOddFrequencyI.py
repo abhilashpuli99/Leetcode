6a12fda42a7a742d1c41c1898fa1a845fcbfe6b0
Abhilash Puli <sherlockholmes9886@gmail.com>
2025-09-02 13:40:42 +0530

    Create 3753-MaximumDifferenceBetweenEvenAndOddFrequencyI

---
+++ 3753-MaximumDifferenceBetweenEvenAndOddFrequencyI/3753-MaximumDifferenceBetweenEvenAndOddFrequencyI.py
@@ -0,0 +1,14 @@
+# Last updated: 9/2/2025, 1:40:44 PM
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        bucket=Counter(s)
+        even=float('inf')
+        odd=0
+        for i in bucket:
+            if bucket[i]%2!=0:
+                odd=max(bucket[i],odd)
+            if bucket[i]%2==0 :
+                even=min(even,bucket[i])
+        return odd-even
+        
+        
